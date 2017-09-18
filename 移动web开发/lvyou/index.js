da0f77846753e60383fd21678108b2d5c17539ed
liuduanyang <1069831692@qq.com>
2017-09-18 21:59:13 +0800

    旅游网站首页pc端

---
+++ 移动web开发/lvyou/index.js
@@ -0,0 +1,8 @@
+        var iouzhou=document.getElementById("iouzhou");
+        var show=document.getElementsByClassName("ouzhou")[0];
+        iouzhou.onmouseover=function(){
+            show.style.display="block";
+        }
+        show.onmouseleave=function(){
+            show.style.display="none";
+        } 
